2048fa84d8afb08cfda311165c5991f0150c574b
ahmadsobri <asobach9@gmail.com>
2021-04-08 13:14:59 +0700

    update rabbitmq project

--- 07-04-2021/connect-rabbitmq/consumer/src/main/java/com/example/consumer/ConsumerConfig.java
+++ 07-04-2021/connect-rabbitmq/consumer/src/main/java/com/example/consumer/ConsumerConfig.java
@@ -6,17 +6,12 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConsumerConfig {
-
-    @Autowired
-    BeanFactory beanFactory;
 
     @Value("${spring.rabbitmq.queue}")
     private String queue;
